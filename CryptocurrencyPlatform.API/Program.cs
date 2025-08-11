564cf95659af5c00f2b55af9f8def294d66618f8
Illya <illamatviuk@gmail.com>
2025-08-11 19:50:56 +0300

    Added Program.cs and appsettings.

--- CryptocurrencyPlatform.API/Program.cs
+++ CryptocurrencyPlatform.API/Program.cs
@@ -1,9 +1,20 @@
+using CryptocurrencyPlatform.Domain.Interfaces.Services;
+using CryptocurrencyPlatform.Infrastructure.Services;
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IAssetService, AssetService>(client => {
+    client.BaseAddress = new Uri("https://rest.coincap.io/v3/");
+
+    client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", builder.Configuration["BearerAuth:Value"]);
+});
 
 var app = builder.Build();
 
