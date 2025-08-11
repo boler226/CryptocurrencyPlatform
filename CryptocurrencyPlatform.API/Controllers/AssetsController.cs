bb0a22ea0cf367b6505ce64eabebe87575d38cab
Illya <illamatviuk@gmail.com>
2025-08-11 19:50:26 +0300

    Added AssetsController.

---
+++ CryptocurrencyPlatform.API/Controllers/AssetsController.cs
@@ -0,0 +1,12 @@
+﻿using CryptocurrencyPlatform.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptocurrencyPlatform.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AssetsController(IAssetService service) : ControllerBase {
+        [HttpGet("{ids}")]
+        public async Task<IActionResult> GetById(string ids, CancellationToken cancellationToken) =>
+            Ok(await service.GetById(ids, cancellationToken));
+    }
+}
