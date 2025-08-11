ab6ae75c316c13ee13218fa2cb2a985587b6a99b
Illya <illamatviuk@gmail.com>
2025-08-11 19:48:26 +0300

    Added AssetEntity.

---
+++ CryptocurrencyPlatform.Domain/Entities/AssetEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptocurrencyPlatform.Domain.Entities {
+    public class AssetEntity {
+        public string Id { get; set; } = null!;
+        public string Rank { get; set; } = null!;
+        public string Symbol { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public decimal Supply { get; set; }
+        public decimal? MaxSupply { get; set; }
+        public decimal MarketCapUsd { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal ChangePercent24Hr { get; set; }
+        public decimal? Vwap24Hr { get; set; }
+        public string Explorer { get; set; } = null!;
+        public Dictionary<string, List<string>>? Tokens { get; set; } = new();
+    }
+}
