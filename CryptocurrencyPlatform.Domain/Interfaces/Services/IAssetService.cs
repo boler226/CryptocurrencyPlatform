1f18885bf62ed71720c8acf24211dc96af8ddd00
Illya <illamatviuk@gmail.com>
2025-08-11 19:49:10 +0300

    Added AssetService with only GetById request.

---
+++ CryptocurrencyPlatform.Domain/Interfaces/Services/IAssetService.cs
@@ -0,0 +1,7 @@
+﻿using CryptocurrencyPlatform.Domain.Entities;
+
+namespace CryptocurrencyPlatform.Domain.Interfaces.Services {
+    public interface IAssetService {
+        Task<List<AssetEntity>> GetById(string Id, CancellationToken cancellationToken);
+    }
+}
---
+++ CryptocurrencyPlatform.Infrastructure/Services/AssetService.cs
@@ -0,0 +1,21 @@
+﻿using CryptocurrencyPlatform.Application.DTOs.Response;
+using CryptocurrencyPlatform.Domain.Entities;
+using CryptocurrencyPlatform.Domain.Interfaces.Services;
+using Newtonsoft.Json;
+
+namespace CryptocurrencyPlatform.Infrastructure.Services {
+    public class AssetService(HttpClient httpClient) : IAssetService {
+        public async Task<List<AssetEntity>> GetById(string Ids, CancellationToken cancellationToken) {
+            var asset = await httpClient.GetAsync($"{httpClient.BaseAddress}/assets?ids={Ids}", cancellationToken)
+                       ?? throw new Exception("Failed to fetch asset");
+
+            var content = await asset.Content.ReadAsStringAsync(cancellationToken);
+
+
+            var entity = JsonConvert.DeserializeObject<DefaultResponseDto<AssetEntity>>(content)
+                         ?? throw new Exception("Failed to deserialize asset");
+
+            return entity.Data;
+        }
+    }
+}
