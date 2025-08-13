87f85720a6aeb92444246fea5cfa400092ce1e04
Illya <illamatviuk@gmail.com>
2025-08-13 15:32:25 +0300

    Added UnauthorizedDelegatingHandler.

---
+++ CryptocurrencyPlatform.Infrastructure/Exceptions/Delegation/Unauthorized/UnauthorizedDelegatingHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using CryptocurrencyPlatform.Infrastructure.Exceptions.Exception.Unauthorized;
+using System.Net;
+
+namespace CryptocurrencyPlatform.Infrastructure.Exceptions.Delegation.Unauthorized {
+    public class UnauthorizedDelegatingHandler : DelegatingHandler {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized || response.StatusCode == HttpStatusCode.Forbidden)
+                throw new UnauthorizedException();
+
+            return response;
+        }
+    }
+}
