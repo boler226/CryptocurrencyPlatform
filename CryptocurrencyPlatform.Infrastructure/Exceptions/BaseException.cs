9f8cab0c9e5dccfc65414dd58b9d4705d2c510e7
Illya <illamatviuk@gmail.com>
2025-08-13 15:31:47 +0300

    Added BaseException.

---
+++ CryptocurrencyPlatform.Infrastructure/Exceptions/BaseException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace CryptocurrencyPlatform.Infrastructure.Exceptions {
+    public class BaseException : System.Exception {
+        public HttpStatusCode StatusCode { get; }
+        public BaseException(string message, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+            : base(message) {
+            StatusCode = statusCode;
+        }
+    }
+}
