1db4f07834169348828201cce4aea470e902c33c
Illya <illamatviuk@gmail.com>
2025-08-13 15:32:05 +0300

    Added UnauthorizedException.

---
+++ CryptocurrencyPlatform.Infrastructure/Exceptions/Exception/Unauthorized/UnauthorizedException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace CryptocurrencyPlatform.Infrastructure.Exceptions.Exception.Unauthorized {
+    public class UnauthorizedException : BaseException {
+        public UnauthorizedException() 
+            : base ("Unauthorized", HttpStatusCode.Unauthorized) { 
+        }
+    }
+}
---
+++ CryptocurrencyPlatform.Infrastructure/Exceptions/Exception/Unauthorized/UnauthorizedExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace CryptocurrencyPlatform.Infrastructure.Exceptions.Exception.Unauthorized {
+    public class UnauthorizedExceptionHandler(
+        ILogger<UnauthorizedException> logger
+        ) : IExceptionHandler {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, System.Exception exception, CancellationToken cancellationToken) {
+            if (exception is not UnauthorizedException e)
+                return false;
+
+            var problemDetails = new ProblemDetails() {
+                Instance = httpContext.Request.Path,
+                Title = exception.Message,
+            };
+
+            httpContext.Response.StatusCode = (int)e.StatusCode;
+
+            logger.LogError("Unauthorized error: {Message}", exception.Message);
+            problemDetails.Status = httpContext.Response.StatusCode;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
