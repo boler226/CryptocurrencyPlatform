9f992a22faf8ae3145fe7787f27317e985a9163f
Illya <illamatviuk@gmail.com>
2025-08-13 15:32:40 +0300

    Added ExceptionExtensions.

---
+++ CryptocurrencyPlatform.Infrastructure/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CryptocurrencyPlatform.Infrastructure.Exceptions.Exception.Unauthorized;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptocurrencyPlatform.Infrastructure.Extensions
+{
+    public static class ExceptionExtensions {
+        public static void AddExceptionHandlers(this IServiceCollection services) {
+            services.AddExceptionHandler<UnauthorizedExceptionHandler>();
+            services.AddProblemDetails();
+        }
+    }
+}
