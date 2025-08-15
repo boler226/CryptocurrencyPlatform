efb086de21e33b94d84c9aa5059e58cc35d7a3a1
Illya <illamatviuk@gmail.com>
2025-08-15 13:47:04 +0300

    Added ViewModelBase.

---
+++ CryptocurrencyPlatform.WPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyPlatform.WPF.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+
+        public virtual void Dispose() { }
+    }
+}
