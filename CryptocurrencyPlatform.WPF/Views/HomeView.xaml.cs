c493b664d0a4e2d196ad74d2f03123a96eac4552
Illya <illamatviuk@gmail.com>
2025-08-15 13:50:50 +0300

    Changed style for HomeView.xaml.

--- CryptocurrencyPlatform.WPF/Views/HomeView.xaml.cs
+++ CryptocurrencyPlatform.WPF/Views/HomeView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using CryptocurrencyPlatform.WPF.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
 
 namespace CryptocurrencyPlatform.WPF.Views {
     public partial class HomeView : UserControl {
