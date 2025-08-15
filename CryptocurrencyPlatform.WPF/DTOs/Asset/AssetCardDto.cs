20ed763236089a17679fef43ab58f92fe0651d73
Illya <illamatviuk@gmail.com>
2025-08-15 13:41:38 +0300

    Added AssetCardDto for WPF.

---
+++ CryptocurrencyPlatform.WPF/DTOs/Asset/AssetCardDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptocurrencyPlatform.WPF.DTOs.Asset {
+    public class AssetCardDto {
+        public string Name { get; set; } = null!;
+        public string Symbol { get; set; } = null!;
+        public decimal PriceUsd { get; set; }
+        public string ChangePercent24Hr { get; set; } = null!;
+    }
+}
