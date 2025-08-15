d55333e1938e8fca9e5b093d160fda41c29f951e
Illya <illamatviuk@gmail.com>
2025-08-15 13:49:59 +0300

    Added HomeViewModel with request to service.

--- CryptocurrencyPlatform.WPF/ViewModels/HomeViewModel.cs
+++ CryptocurrencyPlatform.WPF/ViewModels/HomeViewModel.cs
@@ -1,6 +1,40 @@
-﻿namespace CryptocurrencyPlatform.WPF.ViewModels {
-    public class HomeViewModel {
-        public HomeViewModel() {
+﻿using CryptocurrencyPlatform.Domain.Entities;
+using CryptocurrencyPlatform.Domain.Interfaces.Services;
+using CryptocurrencyPlatform.WPF.Commands;
+using CryptocurrencyPlatform.WPF.DTOs.Asset;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CryptocurrencyPlatform.WPF.ViewModels {
+    public class HomeViewModel : ViewModelBase {
+        private readonly IAssetService _assetService;
+        public ObservableCollection<AssetCardDto> AssetCards { get; set; } = new();
+        public ICommand LoadAssetsCommand { get; }
+
+        public HomeViewModel(IAssetService assetService) {
+            _assetService = assetService;
+            LoadAssetsCommand = new RelayCommand(async _ => await LoadAssets(), _ => true);
+            _ = LoadAssets();
+        }
+
+        public async Task LoadAssets() {
+            try {
+                var assets = await _assetService.GetList(null, CancellationToken.None, 20, 0);
+                AssetCards.Clear();
+                foreach (var asset in assets)
+                    AssetCards.Add(new AssetCardDto {
+                        Name = asset.Name,
+                        Symbol = asset.Symbol,
+                        PriceUsd = Convert.ToDecimal(asset.PriceUsd, System.Globalization.CultureInfo.InvariantCulture),
+                        ChangePercent24Hr = asset.ChangePercent24Hr + "%"
+
+                    });
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "LoadAssets Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
