0102af7ffcc1122da25618eb0a825304f1bb50d2
Illya <illamatviuk@gmail.com>
2025-08-19 13:21:09 +0300

    Added CoincapClientExtensions.

---
+++ CryptocurrencyPlatform.Infrastructure/Extensions/CoincapClientExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CryptocurrencyPlatform.Infrastructure.Exceptions.Delegation.Unauthorized;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptocurrencyPlatform.Infrastructure.Extensions {
+    public static class CoincapClientExtensions {
+        public static void ConfigureCoincapClient(this HttpClient httpClient, IConfiguration configuration) {
+            httpClient.BaseAddress = new Uri("https://rest.coincap.io/v3/");
+            httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", configuration["BearerAuth:Value"]);
+        }
+
+        public static IServiceCollection AddCoincapClient<TClient, TImplementation>(
+            this IServiceCollection services, IConfiguration configuration)
+            where TClient : class
+            where TImplementation : class, TClient {
+            services.AddHttpClient<TClient, TImplementation>(client => {
+                client.ConfigureCoincapClient(configuration);
+            })
+            .AddHttpMessageHandler<UnauthorizedDelegatingHandler>();
+
+            return services;
+        }
+    }
+}
