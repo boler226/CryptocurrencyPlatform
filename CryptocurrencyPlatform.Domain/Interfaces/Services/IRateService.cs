04fe901f2333bf86d63c3e3981e6d5e6abe91b4e
Illya <illamatviuk@gmail.com>
2025-08-19 13:20:23 +0300

    Added RateService with GetById and GetList.

---
+++ CryptocurrencyPlatform.Domain/Interfaces/Services/IRateService.cs
@@ -0,0 +1,8 @@
+﻿using CryptocurrencyPlatform.Domain.Entities;
+
+namespace CryptocurrencyPlatform.Domain.Interfaces.Services {
+    public interface IRateService {
+        Task<RateEntity> GetById(string slug, CancellationToken cancellationToken);
+        Task<List<RateEntity>> GetList(string? ids, CancellationToken cancellationToken);
+    }
+}
---
+++ CryptocurrencyPlatform.Infrastructure/Services/RateService.cs
@@ -0,0 +1,32 @@
+﻿using CryptocurrencyPlatform.Application.DTOs.Response;
+using CryptocurrencyPlatform.Domain.Entities;
+using CryptocurrencyPlatform.Domain.Interfaces.Services;
+using Newtonsoft.Json;
+
+namespace CryptocurrencyPlatform.Infrastructure.Services {
+    public class RateService(HttpClient httpClient) : IRateService {
+        public async Task<RateEntity> GetById(string slug, CancellationToken cancellationToken) {
+            var rate = await httpClient.GetAsync($"{httpClient.BaseAddress}/rates/{slug}", cancellationToken)
+                       ?? throw new Exception("");
+
+            var content = await rate.Content.ReadAsStringAsync(cancellationToken);
+
+            var entity = JsonConvert.DeserializeObject<DefaultResponseDto<RateEntity>>(content)
+                         ?? throw new Exception("Failed to deserialize asset");
+
+            return entity.Data;
+        }
+
+        public async Task<List<RateEntity>> GetList(string? ids, CancellationToken cancellationToken) {
+            var rates = await httpClient.GetAsync($"{httpClient.BaseAddress}/rates?ids={ids}", cancellationToken)
+                                       ?? throw new Exception("");
+
+            var content = await rates.Content.ReadAsStringAsync(cancellationToken);
+
+            var entity = JsonConvert.DeserializeObject<DefaultListResponseDto<RateEntity>>(content)
+                         ?? throw new Exception("Failed to deserialize asset");
+
+            return entity.Data;
+        }
+    }
+}
