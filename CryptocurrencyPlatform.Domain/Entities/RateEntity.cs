b91766d282476764df4baaa8c32c2864575ed4d8
Illya <illamatviuk@gmail.com>
2025-08-19 13:19:57 +0300

    Added RateEntity.

---
+++ CryptocurrencyPlatform.Domain/Entities/RateEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptocurrencyPlatform.Domain.Entities {
+    public class RateEntity {
+        public string Id { get; set; } = null!;
+        public string Symbol { get; set; } = null!;
+        public string CurrencySymbol { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public decimal RateUsd { get; set; }
+    }
+}
