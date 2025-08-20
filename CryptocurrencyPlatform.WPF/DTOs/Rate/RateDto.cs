63f92ef07f49316b1b220e82239d3589476bee8e
Illya <illamatviuk@gmail.com>
2025-08-20 21:05:40 +0300

    Added RateDto.

---
+++ CryptocurrencyPlatform.WPF/DTOs/Rate/RateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptocurrencyPlatform.WPF.DTOs.Rate {
+    public class RateDto {
+        public string Symbol { get; set; } = null!;
+        public string CurrencySymbol { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public decimal RateUsd { get; set; }
+    }
+}
